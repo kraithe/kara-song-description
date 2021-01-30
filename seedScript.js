5cc46190ed4107d1660accabab91f82b99b4dda2
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-01-29 19:26:26 -0500

    Fix seedScript, updated to current version

--- seedScript.js
+++ seedScript.js
@@ -5,23 +5,23 @@ const studios = require('./studioNames.js');
 // then insert chunk
 // next deadline - just upload efficiently
 
-const seedDatabase = async() => {
-  var deleted = await database.deleteDescriptions();
-  for (var i = 0; i < 100; i++) {
-    var descriptionObj = {
-      songId: i + 1,
-      bandId: i + 1,
-      bandName: bands.bandNames[i],
-      description: `Recorded, mixed, and mastered at ${studios.studioNames[i]} Studios. Produced by ${bands.bandNames[i]}. © 2020 ${bands.bandNames[i]}. All rights reserved.`
-    }
-    database.saveDescriptions(descriptionObj)
-      .then((response) => {
-        console.log('Response from saveDescriptions to db: ', response);
-      })
-      .catch((error) => {
-        console.log('Error saving descriptions to db: ', error);
-      });
-  };
-};
+// const seedDatabase = async() => {
+//   var deleted = await database.deleteDescriptions();
+//   for (var i = 0; i < 100; i++) {
+//     var descriptionObj = {
+//       songId: i + 1,
+//       bandId: i + 1,
+//       bandName: bands.bandNames[i],
+//       description: `Recorded, mixed, and mastered at ${studios.studioNames[i]} Studios. Produced by ${bands.bandNames[i]}. © 2020 ${bands.bandNames[i]}. All rights reserved.`
+//     }
+//     database.saveDescriptions(descriptionObj)
+//       .then((response) => {
+//         console.log('Response from saveDescriptions to db: ', response);
+//       })
+//       .catch((error) => {
+//         console.log('Error saving descriptions to db: ', error);
+//       });
+//   };
+// };
 
 seedDatabase();
