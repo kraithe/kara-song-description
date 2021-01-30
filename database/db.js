52452d673cb1873386d1351aaa37115a4fbb3e25
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-01-30 02:46:43 -0500

    Revert port to original

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 // ----------- DB CONNECTION ---------- //
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/descriptions', {
+mongoose.connect('mongodb://localhost/descriptions', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
